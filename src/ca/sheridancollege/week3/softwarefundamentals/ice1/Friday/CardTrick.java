e8b97d2251c0abb15d35b455b2007de649407349
ZiangWangg <78223979+ZiangWangg@users.noreply.github.com>
2021-02-07 22:26:05 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -8,7 +8,12 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+    Card c1 = new Card("Diamonds",13);      
+         String suit = c1.getSuit();
+         System.out.print(suit);
+         
+         int value = c1.getValue();
+         System.out.print(value);   
     }
     
 }
