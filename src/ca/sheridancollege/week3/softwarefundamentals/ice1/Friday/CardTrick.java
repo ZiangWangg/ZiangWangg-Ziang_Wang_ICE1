e2bbdfc1e34041db70869ca6d3dcc0b30b48ff3c
ZiangWangg <78223979+ZiangWangg@users.noreply.github.com>
2021-02-07 22:16:16 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -1,10 +1,10 @@
 
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
-
-/**
- *
- * @author Ronak
- */
+/*
+*Ziang Wang
+*9901565800
+* 
+*/
 public class CardTrick {
     public static void main(String[] args)
     {
